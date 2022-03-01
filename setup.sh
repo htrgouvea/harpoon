639c3d7a3a469c1b1967d005873623716e548700
htrgouvea <hi@heitorgouvea.me>
2022-03-01 17:53:15 -0300

    remove workers from the main project

--- setup.sh
+++ setup.sh
@@ -3,13 +3,5 @@
 docker build -t uranus-database ./rest-server/database/
 docker run -d -p 3306:3306 --name database -e MARIADB_ROOT_PASSWORD=mypassword uranus-database
 
-
-
-
-
-
-
 # docker build --rm --squash -t bing-crawler ./crawlers/bing/
-# docker build --rm --squash -t email-notify ./workers/email-notify
 # docker run -d --name bing bing-crawler
-# docker run -d --name email-notify  email-notify
