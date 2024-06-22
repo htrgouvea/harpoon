1ad1bb06303a1215e69d401d6d405a741aedad03
Giovanni Martins <giovannimartins2000@gmail.com>
2024-06-22 19:10:42 -0300

    postgresql database connector

---
+++ api/database/connector.js
@@ -0,0 +1,12 @@
+const dotenv = require('dotenv')
+const { Pool } = require('pg');
+
+const pool = new Pool({
+  user: process.env.DATABASE_USER,
+  host: process.env.DATABASE_HOST,
+  database: process.env.DATABASE_NAME,
+  password: process.env.DATABASE_PASSWORD,
+  port: process.env.DATABASE_PORT,
+});
+
+module.exports = pool;
